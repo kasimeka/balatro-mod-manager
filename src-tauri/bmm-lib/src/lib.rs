557a35d5dbfceb584ddbb4650f5ad079f57c7846
Ö. Efe D. <67526259+skyline69@users.noreply.github.com>
2025-02-12 23:53:13 +0100

    now is clean

--- src-tauri/bmm-lib/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod balamod;
-pub mod database;
-pub mod mod_collections;
-pub mod lovely;
-pub mod finder;
-pub mod installer;
-pub mod smods_installer;
-pub mod cache;
-pub mod errors;
--- src-tauri/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    tauri_build::build();
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../Resources/bundled_dylibs");
-}
