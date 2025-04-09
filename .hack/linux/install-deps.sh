b4a45584b792714fb32dadf5d05d441520fbda6a
janw4ld <janw4ld@tutanota.com>
2025-04-09 20:11:08 +0200

    docker: lock `libwebkit2gtk` to known good version™

--- .hack/linux/install-deps.sh
+++ .hack/linux/install-deps.sh
@@ -9,10 +9,14 @@ apt-get update && apt-get install -y \
   file \
   xdg-utils \
   \
+  libssl-dev \
   libglib2.0-dev \
   libgtk-3-dev \
-  libjavascriptcoregtk-4.1-dev \
   librsvg2-dev \
   libsoup-3.0-dev \
-  libssl-dev \
-  libwebkit2gtk-4.1-dev
+  libjavascriptcoregtk-4.1-dev=2.44.0-2 \
+  libjavascriptcoregtk-4.1-0=2.44.0-2 \
+  gir1.2-javascriptcoregtk-4.1=2.44.0-2 \
+  libwebkit2gtk-4.1-dev=2.44.0-2 \
+  libwebkit2gtk-4.1-0=2.44.0-2 \
+  gir1.2-webkit2-4.1=2.44.0-2
